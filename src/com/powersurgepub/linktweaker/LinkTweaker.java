80d9c944e610fdb2b46616f8abc209913eeb125f
Herb Bowie <hbowie@mac.com>
2014-06-10 05:11:12 -0700

    Extended Copyright notice.
    Signed-off-by: Herb Bowie <hbowie@mac.com>

--- src/com/powersurgepub/linktweaker/LinkTweaker.java
+++ src/com/powersurgepub/linktweaker/LinkTweaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2013 Herb Bowie
+ * Copyright 2012 - 2014 Herb Bowie
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
